930bfd7de2888d2cc9a6deae9c8c89cccc7ed399
Mikolaj Torz <mptorz@icloud.com>
2018-11-12 02:32:43 +0000

    almost almost done

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
 
-export default class Bookshelf extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    books: PropTypes.array.isRequired,
-    changeShelf: PropTypes.func.isRequired,
-  };
-
-  render() {
-    const { title, books, changeShelf } = this.props;
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map(book => (
-              <Book
-                key={book.id}
-                id={book.id}
-                title={book.title}
-                shelfName={book.shelf}
-                authors={book.authors}
-                cover={book.imageLinks}
-                changeShelf={changeShelf}
-              />
-            ))}
-          </ol>
-        </div>
+const Bookshelf = (props) => {
+  const { title, books, changeShelf } = props;
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map(book => (
+            <Book
+              key={book.id}
+              id={book.id}
+              title={book.title}
+              shelfName={book.shelf}
+              authors={book.authors}
+              cover={book.imageLinks}
+              changeShelf={changeShelf}
+            />
+          ))}
+        </ol>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Bookshelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+  changeShelf: PropTypes.func.isRequired,
+};
+
+export default Bookshelf;
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,54 +1,53 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React,  from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Bookshelf from './Bookshelf';
 
-export default class MainPage extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    changeShelf: PropTypes.func.isRequired,
-  };
+const MainPage = (props) => {
+  const { books, changeShelf } = props;
+  const shelves = [
+    {
+      name: 'currentlyReading',
+      title: 'Currently Reading',
+    },
+    {
+      name: 'wantToRead',
+      title: 'Want to Read',
+    },
+    {
+      name: 'read',
+      title: 'Read',
+    },
+  ];
 
-  render() {
-    const { books, changeShelf } = this.props;
-    const shelves = [
-      {
-        name: 'currentlyReading',
-        title: 'Currently Reading',
-      },
-      {
-        name: 'wantToRead',
-        title: 'Want to Read',
-      },
-      {
-        name: 'read',
-        title: 'Read',
-      },
-    ];
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {shelves.map(shelf => (
-              <Bookshelf
-                key={shelf.name}
-                title={shelf.title}
-                books={books.filter(b => b.shelf === shelf.name)}
-                changeShelf={changeShelf}
-              />
-            ))}
-          </div>
-        </div>
-
-        <Link className="open-search" to="/search">
-          Add a book
-        </Link>
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    );
-  }
-}
+      <div className="list-books-content">
+        <div>
+          {shelves.map(shelf => (
+            <Bookshelf
+              key={shelf.name}
+              title={shelf.title}
+              books={books.filter(b => b.shelf === shelf.name)}
+              changeShelf={changeShelf}
+            />
+          ))}
+        </div>
+      </div>
+
+      <Link className="open-search" to="/search">
+        Add a book
+      </Link>
+    </div>
+  );
+};
+
+MainPage.propTypes = {
+  books: PropTypes.array.isRequired,
+  changeShelf: PropTypes.func.isRequired,
+};
+
+export default MainPage;
